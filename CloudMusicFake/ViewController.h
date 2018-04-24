a1eb243276e4dbd34b4c9a68348936c418806d03
TingAlex <tingalex30@gmail.com>
2018-04-24 19:10:36 +0800

    Initial Commit

---
+++ CloudMusicFake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudMusicFake
+//
+//  Created by OurEDA on 2018/4/24.
+//  Copyright © 2018年 OurEDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
